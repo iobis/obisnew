a3d6d05ed8ecfbeb9f83663700cbd309064edae5
pieterprovoost <pieterprovoost@users.noreply.github.com>
2025-06-04 17:56:51 +0000

    deploy: f75c950f81da41de36999a1cd256f9fb9ce9f4ce

--- fastapi/dataset.py
+++ fastapi/dataset.py
@@ -3,16 +3,32 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, FileSystemLoader
 import requests
+import urllib
 
 router = APIRouter()
 
 templates = Environment(loader=FileSystemLoader("templates"))
 shell_templates = Jinja2Templates(directory="static")
 
+
+def get_quality_statistics(filters: dict):
+    params = urllib.parse.urlencode(filters)
+    api_url = f"https://api.obis.org/statistics/qc?{params}"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        result = response.json()
+        return result
+    except Exception as e:
+        print(e)
+
+
 @router.get("/{dataset_id}", response_class=HTMLResponse)
 async def dataset_page(request: Request, dataset_id: str):
-    api_url = f"https://api.obis.org/dataset/{dataset_id}"
 
+    # dataset metadata
+
+    api_url = f"https://api.obis.org/dataset/{dataset_id}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
@@ -22,8 +38,19 @@ async def dataset_page(request: Request, dataset_id: str):
         print(e)
         raise HTTPException(status_code=404, detail="Dataset not found")
 
+    # quality statistics
+
+    quality_statistics = get_quality_statistics({
+        "datasetid": dataset_id,
+        "dropped": "include",
+        "absence": "include"
+    })
+
+    # render
+
     dataset_block = templates.get_template("dataset.html").render(
-        dataset=dataset
+        dataset=dataset,
+        quality_statistics=quality_statistics
     )
 
     return shell_templates.TemplateResponse(
