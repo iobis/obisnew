ffa683909398d173d697b79ea943529a4991d253
pieterprovoost <pieterprovoost@users.noreply.github.com>
2025-06-05 07:27:15 +0000

    deploy: 9ad2b71752c6b9404e17b36f51f587d5a6d6b2f0

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from node import router as node_router
 from dataset import router as dataset_router
 
 app = FastAPI()
 
+app.include_router(node_router, prefix="/node")
 app.include_router(dataset_router, prefix="/dataset")
---
+++ fastapi/node.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
+import requests
+import urllib
+
+router = APIRouter()
+
+templates = Environment(loader=FileSystemLoader("templates"))
+shell_templates = Jinja2Templates(directory="static")
+
+
+@router.get("/{node_id}", response_class=HTMLResponse)
+async def node_page(request: Request, node_id: str):
+
+    # node metadata
+
+    api_url = f"https://api.obis.org/node/{node_id}"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        response_json = response.json()
+        node = response_json["results"][0]
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404, detail="Node not found")
+
+    # datasets
+
+    api_url = f"https://api.obis.org/dataset?size=10&nodeid={node_id}"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        response_json = response.json()
+        datasets = response_json["results"]
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404, detail="Not found")
+
+    # render
+
+    block = templates.get_template("node.html").render(
+        node=node,
+        datasets=datasets
+    )
+
+    return shell_templates.TemplateResponse(
+        request=request,
+        name="portal.html",
+        context={
+            "title": node["name"],
+            "content": block
+        }
+    )
