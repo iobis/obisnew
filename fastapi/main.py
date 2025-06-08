274827eff47d54d55eb9ded570029b8ce702c575
pieterprovoost <pieterprovoost@users.noreply.github.com>
2025-06-08 09:04:49 +0000

    deploy: b87e6d772fe52263b0fd4e0c99f2b21f90bcfa2f

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from node import router as node_router
 from dataset import router as dataset_router
+from taxon import router as taxon_router
 
 app = FastAPI()
 
 app.include_router(node_router, prefix="/node")
 app.include_router(dataset_router, prefix="/dataset")
+app.include_router(taxon_router, prefix="/taxon")
