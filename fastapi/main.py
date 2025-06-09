6f349dd13b5687f21815347a3ca8711278e6bd62
Pieter Provoost <pieterprovoost@gmail.com>
2025-06-09 16:37:07 +0200

    dataset table fixes

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from node import router as node_router
 from dataset import router as dataset_router
 from taxon import router as taxon_router
+from area import router as area_router
 
 app = FastAPI()
 
 app.include_router(node_router, prefix="/node")
 app.include_router(dataset_router, prefix="/dataset")
 app.include_router(taxon_router, prefix="/taxon")
+app.include_router(area_router, prefix="/area")
