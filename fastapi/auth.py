4167f41e4f892e8b4cd73bced7e27716147378a6
Pieter Provoost <pieterprovoost@gmail.com>
2025-07-02 19:58:10 +0200

    orcid debugging

--- fastapi/auth.py
+++ fastapi/auth.py
@@ -64,13 +64,14 @@ async def auth(request: Request, provider: str):
     elif provider == "orcid":
         user = {}
         userinfo = token.get("userinfo")
+        print("USER INFO", userinfo)
         if userinfo:
             user["orcid"] = userinfo.get("sub")
             user["name"] = userinfo.get("name")
             user["email"] = userinfo.get("email")
         else:
-            # fallback: try to get from id_token claims if available
             claims = token.get("id_token_claims")
+            print("CLAIMS", claims)
             if claims:
                 user["orcid"] = claims.get("sub")
                 user["name"] = claims.get("name")
